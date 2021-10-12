81385f0cf7a2cab0f653c101d3af0dbe9c2611d5
kjh910 <god12nom@gmail.com>
2021-10-12 16:18:11 +0900

    fix bug

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,7 +1,9 @@
 const messageList = document.querySelector("ul");
 const nickForm = document.querySelector("#nick");
 const messageForm = document.querySelector("#message");
-const socket = new WebSocket(`ws://localhost:3000`);
+// const socket = new WebSocket(`ws://localhost:3000`);
+const socket = new WebSocket(`wss://${window.location.host}`);
+
 
 function makeMessage(type, payload) {
   const msg = { type, payload };
